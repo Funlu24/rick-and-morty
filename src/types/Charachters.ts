1177aeeff064a48fea03278d927252695372702f
Funlu24 <funlu8748@gmail.com>
2025-01-24 09:13:45 +0300

    fix: correct spelling of 'Character' and enhance type safety with additional properties

--- src/types/Charachters.ts
+++ src/types/Charachters.ts
@@ -1,10 +1,18 @@
 // karakter tipleri burda tanımlanacak
 // ve export edilecek
 
-export interface Charachter {
+/*export interface Character {
   id: number;
   name: string;
-  status: string;
+  status: "alive" | "dead" | "unknown"; // Özel type güvenliği
   species: string;
   type: string;
+  gender: string;
+  origin: { name: string; url: string };
+  location: { name: string; url: string };
+  image: string;
+  episode: string[];
+  url: string;
+  created: string;
 }
+*/
